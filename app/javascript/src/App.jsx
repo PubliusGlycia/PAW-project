5b35d850411ccc5f5ca7b8e7a66ff337820afcb0
rozer1 <robson.rs@vp.pl>
2019-11-07 15:14:16 +0100

    Bootstrap

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
 import { fetchBoards } from './APIs/boards';
 import BoardCreate from './components/BoardCreate';
+import { Button } from 'reactstrap';
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 			this.state = {
-			  boards: []
+				boards: [],
+				toggle: true
 			};
 		this.addBoardToList = this.addBoardToList.bind(this);
+		this.renderCreateForm = this.renderCreateForm.bind(this);
+
 	};
 	
 	addBoardToList(newBoard) {
 		this.setState({
 		   boards : [newBoard, ...this.state.boards],
 		}); 
-	  }
+	  };
 	
   refreshBoards = async () => {
     const boards = await fetchBoards();
@@ -28,10 +32,20 @@ export default class App extends Component {
     await this.refreshBoards();
   };
 
+  renderCreateForm = () => {
+    if (this.state.toggle === true) {
+      return(
+        <div>
+         <BoardCreate onSubmit={this.addBoardToList} />
+        </div>
+      );
+    }
+  };
+
   render() {
     return (
-    	<div className="">
-    		<div className="">
+    	<div className="container">
+    		<div className="mt-5">
     			Choose Board
     			{this.state.boards.map((board, i) => (
 			     	<div key={i}>
@@ -39,9 +53,13 @@ export default class App extends Component {
 			       </div>
 			    ))}
 				</div>
+					<button onClick={this.toggleForm}> 
+					click me
+					</button>
 				<div>
-					<BoardCreate onSubmit={this.addBoardToList} />
+					{this.renderCreateForm()}
 				</div>
+				<Button color="primary">hello</Button>
 			</div>
 		);
   }
