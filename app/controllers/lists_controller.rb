6e801d953e086ef10f77026efd9b547db7b17eda
PubliusGlycia <209415@edu.p.lodz.pl>
2019-11-10 19:57:40 +0100

    [Add][Backend]Update and destroy functions for lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+    before_action :set_list, only: %i[update destroy]
+
     def index
         @board = Board.find(params[:board_id])
         @lists = @board.lists.order(created_at: :desc)
@@ -18,9 +20,23 @@ class ListsController < ApplicationController
         end
     end
 
+    def update
+        @list.update(list_params)
+        render json: @list
+    end
+
+    def destroy
+        @list.destroy
+        render json: @list
+    end
+
     private
 
     def list_params
         params.require(:list).permit(:title, :board_id)
+    end
+
+    def set_list
+        @list = List.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :boards, only: %i[index create update destroy] do
-    resources :lists, only: %i[create index]
+    resources :lists, only: %i[index create update destroy]
   end
   
 end
