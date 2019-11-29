e492f340e14419f222c6b62832561384a4714363
Damian <Damian_golema@interia.eu>
2019-11-29 21:29:31 +0100

    [ADD][Backend] Comments
    should by good :)

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,41 @@
+class CommentController < ApplicationController
+    before_action :set_comment, only: %i[update destroy]
+    def index
+      @card = Card.find(params[:card_id])
+      @comments = @card.comments.order(created_at: :desc)
+      respond_to do |format|
+        format.json do
+          render json: @comment
+        end
+      end
+    end
+  
+    def create
+      comment = Comment.create(comment_params)
+      respond_to do |format|
+        format.json do
+          render json: comment
+        end
+      end
+    end
+  
+    def update
+      @comment.update(comment_params)
+      render json: @comment
+    end
+  
+    def destroy
+      @comment.destroy
+      render json: @comment
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:comment, :card_id)
+    end
+  
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_095630) do
+ActiveRecord::Schema.define(version: 2019_11_29_175842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.bigint "user_id"
   end
 
   create_table "cards", force: :cascade do |t|
     t.bigint "list_id", null: false
     t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "description"
     t.index ["list_id"], name: "index_cards_on_list_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "card_id", null: false
+    t.string "email"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["card_id"], name: "index_comments_on_card_id"
   end
 
   create_table "lists", force: :cascade do |t|
     t.bigint "board_id", null: false
     t.string "title", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["board_id"], name: "index_lists_on_board_id"
-  end
-
-  create_table "pages", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,6 +61,4 @@ ActiveRecord::Schema.define(version: 2019_11_21_095630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "cards", "lists"
-  add_foreign_key "lists", "boards"
 end
