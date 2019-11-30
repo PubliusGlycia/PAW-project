4fc626789e2c4168e237828d3ff6d8a83b7bda28
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2019-11-30 14:13:43 +0100

    Revert "Merge add,update,delete cards"

--- app/javascript/src/components/card/CardShow.jsx
+++ app/javascript/src/components/card/CardShow.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import CardEdit from './CardEdit';
-import CardDelete from './CardDelete';
+import CardEdit from './CardEdit'
+import CardDelete from './CardDelete'
+import CardCreate from '../card/CardCreate'
+import CardIndex from '../card/CardIndex'
+import { fetchCards } from '../../APIs/cards'
 
 export default class CardShow extends React.Component {
 	constructor(props) {
@@ -25,6 +28,20 @@ export default class CardShow extends React.Component {
 		})
 	  }
 
+	componentDidMount = async () => {
+		await this.refreshCards();
+	  }
+	
+	refreshCards = async () => {
+		const board_id = this.props.board_id 
+		const list_id = this.props.list_id
+		const cards = await fetchCards(board_id, list_id);
+		this.setState({
+		  cards,
+		}); 
+	  }
+
+
 	render() {
 		const cardStyle = {
 			backgroundColor: '#efefef',
@@ -42,7 +59,7 @@ export default class CardShow extends React.Component {
 		<div className="d-flex">
 			<CardEdit 
 				card={card}
-				card_id = {this.props.card.id}
+				card_id={this.props.card_id}
 				board_id={this.props.board_id}
 				list_id={this.props.list_id} 
 				editable={this.state.editable}
@@ -53,9 +70,9 @@ export default class CardShow extends React.Component {
 			<CardDelete
 				board_id={this.props.board_id}
 				list_id={this.props.list_id}
-				card_id={this.props.card.id}
+				card_id={this.props.card_id}
 				card={card}
-				deleteCard={this.props.deleteCard}
+				deleteCard={this.props.card.deleteCard}
 	  		/>
 		</div>
   		</div>
