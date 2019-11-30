667fb21241ceb0672f43898ee8945fb4e9351217
PubliusGlycia <209415@edu.p.lodz.pl>
2019-11-30 14:03:04 +0100

    [Add][Backend]Attachments using Active Storage

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,13 @@
 class Card < ApplicationRecord
   belongs_to :list
+
+  validate :file_size_have_to_be_less_than_5mb, on: :create
+  
+  has_many_attached:image
+
+  def file_size_have_to_be_less_than_5mb
+    return false unless images.attached?
+
+    images.attachments.each { |photo| errors.add(:images, 'can\'t be greater than 5MB') if photo.byte_size > 5_000_000 }
+  end
 end
