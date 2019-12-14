7adcb51f41983dfc6151f2b31ab45646dc0b4f4f
Damian <Damian_golema@interia.eu>
2019-12-14 23:38:49 +0100

    [Front] Poprawa Layout'u

--- app/javascript/src/components/Nav.jsx
+++ app/javascript/src/components/Nav.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import { Navbar, NavItem, NavbarBrand } from 'reactstrap';
+import Logo from "images/logo.jpg";
 
 export default class Nav extends Component {
   render() {
+    const navStyle = {
+      backgroundColor: '#216D95',
+      color:'#FFFFFF',
+      position: 'fixed',
+      top: '0px',
+      width: '100%',
+      paddingTop: '2px'
+    };
+
     return (
-        <Navbar style={{backgroundColor: '#eee6ff'}} className="mb-5">
+        <Navbar style={navStyle}>
           <div className="container">
-            <NavbarBrand href="/">Home</NavbarBrand>
+            <NavbarBrand href="/"><img src={Logo} height="50px"/></NavbarBrand>
           </div>
         </Navbar>
     );
