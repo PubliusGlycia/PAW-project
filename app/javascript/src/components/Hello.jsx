ac6da97c4bac716b6b53099473d48f35292869e5
rozer1 <robson.rs@vp.pl>
2019-12-15 13:54:07 +0100

    Login style update

--- app/javascript/src/components/Hello.jsx
+++ app/javascript/src/components/Hello.jsx
@@ -3,8 +3,13 @@ import { Route, Switch } from 'react-router-dom'
 
 export default class Hello extends Component {
   render() {
+    const HelloStyle = {
+      position: 'fixed',
+      width: '100%',
+    };
+
     return (
-      <div>
+      <div style={HelloStyle}>
         <h1>Hello!</h1>
       </div>
     );
