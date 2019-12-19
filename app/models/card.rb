fb74ebd1afe4bf6108b79b791c28462d627b67f5
PubliusGlycia <209415@edu.p.lodz.pl>
2019-12-19 10:11:20 +0100

    [Add][Backend]Attachements in comments

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,6 +3,7 @@ class Card < ApplicationRecord
   has_many :comments
 
   validate :file_size_have_to_be_less_than_5mb, on: :create
+  validate :file_format_jpg_jpeg_png, on: :create
 
   has_many_attached :images
 
@@ -11,4 +12,13 @@ class Card < ApplicationRecord
 
     images.attachments.each { |photo| errors.add(:images, 'can\'t be greater than 5MB') if photo.byte_size > 5_000_000 }
   end
+
+  def file_format_jpg_jpeg_png
+    return false unless images.attached?
+
+    images.attachments.each do |photo|
+      if photo.content_type != 'image/png' && photo.content_type != 'image/jpg' && photo.content_type != 'image/jpeg'
+        errors.add(:images, 'image needs to be png / jpeg / jpg')
+      end
+    end
 end
